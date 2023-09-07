7810586db8deebc0be088a7cd9f1d6442e441805
tuokall <tuokall@jyu.fi>
2023-09-07 12:42:34 +0300

    database creation started

--- src/databaseconnector/DataBaseConnector.py
+++ src/databaseconnector/DataBaseConnector.py
@@ -1,8 +1,53 @@
+import configparser
+import os
+import sqlite3
 
+class iniFileError(Exception):
+    pass
+
+class DBError(Exception):
+    pass
 
 class DataBaseConnector:
 
-    def ReadIni():
+    def CreateDB(self, name, inipath):
+        if os.path.exists(name):
+            raise DBError(f'File {name} exists, will not overwrite')
+        self.ReadIni(inipath)
+        filename = (f'file:{name}?mode=rwc')
+        connection = sqlite3.connect(filename,uri=True)
+        return connection
+
+
+    def ReadMapInfo(self, inipath, dbIniConnector):
+        map = dict()
+        if dbIniConnector.has_section('map'):
+            map_areas = dbIniConnector.get('map','areas').split(',')
+            for area in map_areas:
+                area=area.strip()
+                if dbIniConnector.has_option(area,'zones'):
+                    map[area]=dbIniConnector.get(area,'zones').split(',')
+                else:
+                    raise iniFileError(f'Ini file {inipath} map info is inconsistent')
+                    
+        else:
+            raise iniFileError(f'Ini file {inipath} does not have map info')
+        return map
+
+
+    def ReadIni(self, inipath):
+        dbIni = configparser.ConfigParser()
+        try:
+            dbIni.read_file(open(inipath))
+        except Exception as err:
+            raise iniFileError(f'Unable to open {inipath}')
+        if dbIni.has_option('metadata','use'):
+            if not dbIni.get('metadata','use') == 'dbinit':
+                raise iniFileError(f'Initfile {inipath} is not for database init')
+        else:
+            raise iniFileError(f'Initfile {inipath} does not have use defined')
+        
+        map = self.ReadMapInfo(inipath,dbIni)
+
         return
-    
     
--- src/unit_testing/testDataBaseConnector.py
+++ src/unit_testing/testDataBaseConnector.py
@@ -1,11 +1,33 @@
 import unittest
 import databaseconnector.DataBaseConnector as DBC
+import sqlite3
+import os
 
 class testDataBaseConnector(unittest.TestCase):
     def setUp(self):
         self.connector = DBC.DataBaseConnector()
         filepath = 'unit_testing/testdata/'
         self.noIni = filepath+'nofile'
+        self.wrongIni = filepath+'wrongDB.ini'
+        self.wrongMap = filepath+'wrongMapInfoDB.ini'
+        self.correctIni = filepath+'correctDB.ini'
+        self.existingDB = filepath+'existing.DB'
+
 
     def test_noIniFile(self):
-        self.assertRaises(DBC.iniFileError, DBC.CreateDB,self.noIni)
+        self.assertRaises(DBC.iniFileError, self.connector.CreateDB, 'name.db',self.noIni)
+
+    def test_incorrectIniFile(self):
+        self.assertRaises(DBC.iniFileError, self.connector.CreateDB, 'name.db', self.wrongIni)
+    
+    def test_incorrectMapInfo(self):
+        self.assertRaises(DBC.iniFileError, self.connector.CreateDB, 'name.db', self.wrongMap)
+
+    def test_correctIniFile(self):
+        connection = self.connector.CreateDB('name.db', self.correctIni)
+        self.assertIsInstance(connection, sqlite3.Connection)
+        if os.path.exists('name.db'):
+            os.remove('name.db')
+
+    def test_overwriteExistingDB(self):
+        self.assertRaises(DBC.DBError, self.connector.CreateDB, self.existingDB, self.correctIni)
